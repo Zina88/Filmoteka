fbacf0a7b0994217d03d5f60301dccb935f04357
Anna-Chumakova <ann020187@gmail.com>
2022-08-27 08:53:33 +0300

    добавляет карту видео, пробная версия

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,41 @@
 import '../sass/main.scss';
 // import axios from 'axios';
+const refs = {
+    gallery: document.querySelector(".gallery")
+};
+
+
+
+
+
+
+
+
+
+
+
+function appendMoviesMarkup(Array) {
+    refs.gallery.insertAdjacentHTML("beforeend", createMoviesMarkup(Array));   
+}
+function createMoviesMarkup(Array) {
+    const moviesMarkup = Array.map(({
+        title,
+        name,
+        release_date: releaseDate,
+        first_date: firstDate,
+        genre_ids: genresOfMovie,
+        poster_path: poster = "images/plug-image.png",
+        poster: poster = "images/"
+    }) => `<a href="${poster}">
+    <li>
+        <img src="${poster}" alt="${title}">
+        <p>${title}</p>
+        <div>
+            <p>${genresOfMovie}</p>
+            <p>${firstDate}</p>
+        </div>
+        
+    </li>
+</a>`).join("")
+    return moviesMarkup;
+}
