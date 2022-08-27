06bf4a64d99c40455025bf4ae31da887ba17d307
Zina <zinasaenko@hotmail.com>
2022-08-27 15:09:31 +0300

    fix

--- src/js/markupCard.js
+++ src/js/markupCard.js
@@ -1,18 +1,20 @@
-import getGenresFromLocal from "./getGenresFromLocal";
-export const refs = {
-    gallery: document.querySelector(".gallery")
+import getGenresFromLocal from './getGenresFromLocal';
+const refs = {
+  gallery: document.querySelector('.gallery'),
 };
+
 export function appendMoviesMarkup(Array) {
-    refs.gallery.insertAdjacentHTML("beforeend", createMoviesMarkup(Array));   
+  refs.gallery.insertAdjacentHTML('beforeend', createMoviesMarkup(Array));
 }
 export function createMoviesMarkup(Array) {
-    const moviesMarkup = Array.map(({
-        title,
-        name,
-        release_date: releaseDate,
-        first_date: firstDate,
-        genre_ids: genresOfMovie,
-        poster_path: poster = "images/plug-image.png",
+  const moviesMarkup = Array.map(
+    ({
+      title,
+      name,
+      release_date: releaseDate,
+      first_date: firstDate,
+      genre_ids: genresOfMovie,
+      poster_path: poster = 'images/plug-image.png',
     }) => `<li class="card-item">
         <a class="card-link" href="https://image.tmdb.org/t/p/w500${poster}">
         <img class="card__image" src="https://image.tmdb.org/t/p/w500${poster}" alt="${title}">
@@ -20,10 +22,13 @@ export function createMoviesMarkup(Array) {
         <p class="card-title">${title ? title : name}</p>
         <ul class="box">
             <li class="card-genres">${getGenresFromLocal(genresOfMovie)}</li>
-            <li class="card-data">${releaseDate ? releaseDate.slice(0,4) : firstDate.slice(0,4)}</li>
+            <li class="card-data">${
+              releaseDate ? releaseDate.slice(0, 4) : firstDate.slice(0, 4)
+            }</li>
         </ul>
         </div>
         </a>    
-    </li>`).join("")
-    return moviesMarkup;
+    </li>`
+  ).join('');
+  return moviesMarkup;
 }
