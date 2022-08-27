26fea15d387e1f96c4712fae23cbf94d1b1373a5
dimon4uk1990 <mexal07@ukr.net>
2022-08-27 21:03:59 +0300

    Правки спінера

--- src/js/header.js
+++ src/js/header.js
@@ -1,13 +1,8 @@
-refs = {
-  library: document.querySelector('[data-name="library"]'),
-  home: document.querySelector('[data-name="home"]'),
-};
-
-refs.library.addEventListener('click', onClickLibrary);
-refs.home.addEventListener('click', onClickHome);
-function onClickLibrary(e) {
-  window.location.href = 'http://localhost:1234/library.html';
-}
-function onClickHome(e) {
-  window.location.href = 'http://localhost:1234/index.html';
-}
+// function goToPage() {
+//   const url = document.getElementById('id');
+//   document.location.href = url.velue;
+// }
+// const refs = {
+//   library: document.querySelector('data-name=[library]'),
+// };
+// refs.library.addEventListener('click', goToPage());
