4146443baa8c2eeadcc4020b864a3a600a53160c
Oksana Bulakh <ksushavin15@gmail.com>
2022-08-29 14:26:04 +0200

    вказує правильний шлях до  getMovieFromLocal у рендері модалки

--- src/js/getGenresFromLocal.js
+++ src/js/getGenresFromLocal.js
@@ -5,13 +5,10 @@ import { STORAGE_KEY_GENRES } from './constants';
 
 export default function getGenresFromLocal(genreIdsArray) {
   const genresArrayFromLocal = getFromLocal(STORAGE_KEY_GENRES);
- 
   let genresOfMovie = [];
-
   genreIdsArray.forEach(id => {
     const genreOfMovie = genresArrayFromLocal.find(genre => genre.id === id);
     genresOfMovie.push(genreOfMovie.name);
   });
-  // console.log(genresOfMovie);
   return genresOfMovie.join(', ');
 }
