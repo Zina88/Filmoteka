6e6c7af8fd57091c357640c5c7d0d51b3bfb245b
Zina <zinasaenko@hotmail.com>
2022-08-29 10:22:03 +0300

    modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,39 +1,42 @@
-// const openModal = document.querySelector('.test')
-// const backdrop = document.querySelector('.backdrop')
-// const closeModal = document.querySelector('.material-icons')
-// const modal = document.querySelector('[data-index="backdrop"]')
+const refs = {
+  gallery: document.querySelector('.gallery'),
+  openModal: document.querySelector('[data-modal]'),
+  closeModal: document.querySelector('[data-action="close-modal"]'),
+  backdrop: document.querySelector('.backdropMovie'),
+};
 
-// openModal.addEventListener('click', onModalOpen);
-// closeModal.addEventListener('click', onModalClose);
-// backdrop.addEventListener('click', onBackdropCloseModal);
+refs.gallery.addEventListener('click', onGalleryClick);
+refs.openModal.addEventListener('click', onOpenModal);
+refs.backdrop.addEventListener('click', onBackdropClick);
+refs.closeModal.addEventListener('click', onCloseModal);
 
+function onGalleryClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG' && e.target.nodeName !== 'P') {
+    return;
+  }
+  console.dir(e.target);
+  onOpenModal();
+}
 
+function onOpenModal() {
+  console.log('open');
+  refs.backdrop.classList.toggle('is-hidden');
+}
 
-// function onModalOpen(){
-//     modal.classList.remove('is-hidden')
-//     window.addEventListener('keydown', onTargetKeydown);
+function onCloseModal() {
+  console.log('open');
+  refs.backdrop.classList.toggle('is-hidden');
+}
+
+// function onBackdropClick() {
+//   if (e.currentTarget === e.target) {
+//     onCloseFooterModal();
+//   }
 // }
 
-// function onModalClose(){
-//     modal.classList.add('is-hidden')
+//   if (isEscKey) {
+//     onCloseFooterModal();
 //     window.removeEventListener('keydown', onTargetKeydown);
+//   }
 // }
-
-// function onTargetKeydown(e) {
-//     const ESC_KEY_CODE = 'Escape';
-//     const isEscKey = e.code === ESC_KEY_CODE;
-  
-//     if (isEscKey) {
-//         onModalClose();
-//       window.removeEventListener('keydown', onTargetKeydown);
-//     }
-//   }
-
-
-
-//   function onBackdropCloseModal(e) {
-//     if (e.currentTarget === e.target) {
-//         onModalClose();
-//     }
-//   }
-  
