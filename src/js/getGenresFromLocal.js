251c7faac4e38f0a0d15068fd481c069ae3d0e82
Zina <zinasaenko@hotmail.com>
2022-08-29 18:29:50 +0300

    try...catch

--- src/js/getGenresFromLocal.js
+++ src/js/getGenresFromLocal.js
@@ -4,11 +4,15 @@ import getFromLocal from './getFromLocal';
 import { STORAGE_KEY_GENRES } from './constants';
 
 export default function getGenresFromLocal(genreIdsArray) {
-  const genresArrayFromLocal = getFromLocal(STORAGE_KEY_GENRES);
-  let genresOfMovie = [];
-  genreIdsArray.forEach(id => {
-    const genreOfMovie = genresArrayFromLocal.find(genre => genre.id === id);
-    genresOfMovie.push(genreOfMovie.name);
-  });
-  return genresOfMovie.join(', ');
+  try {
+    const genresArrayFromLocal = getFromLocal(STORAGE_KEY_GENRES);
+    let genresOfMovie = [];
+    genreIdsArray.forEach(id => {
+      const genreOfMovie = genresArrayFromLocal.find(genre => genre.id === id);
+      genresOfMovie.push(genreOfMovie.name);
+    });
+    return genresOfMovie.join(', ');
+  } catch (error) {
+    console.log(error);
+  }
 }
