83c39eb0ab8ddc1261a7d1bca339606f880f298e
Kristian <kris.dziamka@gmail.com>
2022-08-30 21:02:57 +0300

    toogle on btn

--- src/js/modal.js
+++ src/js/modal.js
@@ -8,7 +8,8 @@ const refs = {
   openModal: document.querySelector('.modal[data-modal]'),
   closeModal: document.querySelector('[data-action="close-modal"]'),
   backdrop: document.querySelector('.backdropMovie'),
-  modal: document.querySelector('.movie-modal-wrap')
+  modal: document.querySelector('.movie-modal-wrap'),
+
 };
 
 refs.gallery.addEventListener('click', onGalleryClick);
@@ -54,3 +55,9 @@ function onTargetKeydown(e) {
     onCloseModal();
   }
 }
+
+
+
+
+
+
