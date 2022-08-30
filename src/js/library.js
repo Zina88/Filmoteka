98c9805a73d8d999bb70685bfd696e7c59fbc1be
dimon4uk1990 <mexal07@ukr.net>
2022-08-30 07:45:49 +0300

    fix librali

---
+++ src/js/library.js
@@ -0,0 +1,11 @@
+import '../sass/main.scss';
+import './footer-modal';
+import './loader';
+
+import './modal.js';
+import './getGenresFromLocal';
+import './getFromLocal';
+import './getMovieFromLocal';
+import './modalCardOpen';
+import './saveInLocalStorage';
+import '../js/displeyLibrary.js';
