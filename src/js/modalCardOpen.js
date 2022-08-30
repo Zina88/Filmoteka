1738d3c30677921dbda3056fb156a5dcbf41a431
the-most-trained-trainee <oleg.lavrenko@outlook.com>
2022-08-30 23:15:23 +0300

    Pagination Fix

--- src/js/modalCardOpen.js
+++ src/js/modalCardOpen.js
@@ -6,19 +6,32 @@ import saveOnLocalStorage from './saveInLocalStorage';
 import getGenresFromLocal from './getGenresFromLocal';
 
 const refs = {
-  titleEl: document.querySelector('.movie__title'),
-  voteEl: document.querySelector('.vote-value'),
-  votesEl: document.querySelector('.votes-value'),
-  popularityEl: document.querySelector('.movie__info-wrap-item-popularity'),
-  origTitleEl: document.querySelector('.movie__info-wrap-item-name'),
-  genreEl: document.querySelector('.movie__info-item'),
-  aboutEl: document.querySelector('.movie-description'),
-};
+    titleEl: document.querySelector('.movie__title'),
+    voteEl: document.querySelector('.vote-value'),
+    votesEl: document.querySelector('.votes-value'),
+    popularityEl: document.querySelector('.movie__info-wrap-item-popularity'),
+    origTitleEl: document.querySelector('.movie__info-wrap-item-name'),
+    genreEl: document.querySelector('.movie__info-item'),
+    aboutEl: document.querySelector('.movie-description'),
+}
 
 export function openMovieCard(evt) {
-  const clickOnCard = evt.target;
-  const movieId = Number(clickOnCard.id);
-  const movie = getMovieFromLocal(STORAGE_KEY_MOVIES, movieId);
+    
+    const clickOnCard = evt.target;
+    const movieId = Number(clickOnCard.id);
+    const movie = getMovieFromLocal(STORAGE_KEY_MOVIES, movieId);
+    
+    const {
+        id: movieIdFromLocal,
+        poster_path: poster,
+        title: titleMovie,
+        vote_average: vote,
+        vote_count: votes,
+        popularity: popularity,
+        original_title: originalTitle,
+        genre_ids: genreIds,
+        overview: about,
+    } = movie;
 
     saveOnLocalStorage(STORAGE_KEY_MOVIEID, movieIdFromLocal);
 
@@ -32,16 +45,4 @@ export function openMovieCard(evt) {
     refs.origTitleEl.textContent = originalTitle;
     refs.genreEl.textContent = genres;
     refs.aboutEl.textContent = about;
-}
-
-
-  const moviePoster = `https://image.tmdb.org/t/p/w500${poster}`;
-  document.img.src = moviePoster;
-  refs.titleEl.textContent = titleMovie;
-  refs.voteEl.textContent = vote;
-  refs.votesEl.textContent = votes;
-  refs.popularityEl.textContent = popularity;
-  refs.origTitleEl.textContent = originalTitle;
-  refs.genreEl.textContent = getGenresFromLocal(genreIds);
-  refs.aboutEl.textContent = about;
 }
