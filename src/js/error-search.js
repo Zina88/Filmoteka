cba1310d1da0b7149750cea8e1fda21cd9438dd8
dimon4uk1990 <mexal07@ukr.net>
2022-09-01 11:30:17 +0300

    fix стилі модалки футер

--- src/js/error-search.js
+++ src/js/error-search.js
@@ -5,6 +5,5 @@ export default function errorSearch(text) {
   refs.errorEl.classList.remove('visually-hidden');
   setTimeout(() => {
     refs.errorEl.classList.add('visually-hidden');
-  }, 2000);
+  }, 3000);
 }
-// errorSearch('Search result not successful. Enter the correct movie name.');
